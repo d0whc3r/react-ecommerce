1410e8bcdd6a0e9641695e0fddafde5f90d4818b
d0whc3r <d0whc3r@gmail.com>
2020-08-08 10:47:29 +0200

    fix: create collection preview

---
+++ src/components/collections-overview/collections-overview.component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './collections-overview.styles.scss';
+import { connect } from 'react-redux';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+import PropTypes from 'prop-types';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+import { createStructuredSelector } from 'reselect';
+import { RootState } from '../../redux/root-reducer';
+
+type CollectionsOverviewProps = ReturnType<typeof mapStateToProps>;
+
+const CollectionsOverview: React.FC<CollectionsOverviewProps> = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+CollectionsOverview.propTypes = {
+  collections: PropTypes.array.isRequired
+};
+
+interface MapStateToPropsSelector {
+  collections: ReturnType<typeof selectCollections>;
+}
+
+const mapStateToProps = createStructuredSelector<RootState, MapStateToPropsSelector>({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.tsx
+++ src/pages/shop/shop.component.tsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import { RootState } from '../../redux/root-reducer';
-import { connect } from 'react-redux';
-
-type ShopPageProps = ReturnType<typeof mapStateToProps>;
-
-const ShopPage: React.FC<ShopPageProps> = ({ collections }) => (
-  <div>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+const ShopPage: React.FC = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
   </div>
 );
 
-ShopPage.propTypes = {
-  collections: PropTypes.array.isRequired
-};
-
-interface MapStateToPropsSelector {
-  collections: ReturnType<typeof selectCollections>;
-}
-
-const mapStateToProps = createStructuredSelector<RootState, MapStateToPropsSelector>({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
